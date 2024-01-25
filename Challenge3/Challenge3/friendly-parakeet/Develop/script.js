1f1868e2e80f8e76e642a6dfbb0ae1832eba8a34
NatEngs <153305062+NatEngs@users.noreply.github.com>
2024-01-24 20:15:59 -0600

    Fixed typos and moved html file
    Can't get the page to go live in url.  Troubleshooting.

---
+++ Challenge3/Challenge3/friendly-parakeet/Develop/script.js
@@ -0,0 +1,62 @@
+// Assignment Code (provided)
+var generateBtn = document.querySelector("#generate");
+
+//This is the prompt to determine criteria for password input
+function generatePassword() {
+  var passLength = prompt("Your password must be between 8 to 128 characters long.  Please enter your requested length of password.");
+
+  //If statment for password compliance to criteria for entering number
+  if (isNaN(passLength)) {
+    alert("Please enter a number of desired length.");
+    return
+  }
+
+  //If statement for password compliace for length selected.
+  if (passLength < 8 || passLength > 128) {
+    alert("Your requested password length does not meet the criteria.");
+    return
+  }
+
+  //Confirm prompts of additional password criteria.
+  var upCase = confirm("Your password must include at least one capital letter.");
+  var lowCase = confirm("Your password must contain at least one lowercase letter.");
+  var numCase = confirm("Your password must contain at least one number.");
+  var specialCase = confirm("Your password must contain at least one special character.")
+
+  //This will determine what can be used in password
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var special = "~!@#$%^&*()";
+  var multiSelect = "";
+
+  //Call the criteria to create password 
+  if (upCase) {
+    multiSelect += uppercase;
+  }
+  if (lowCase) {
+    multiSelect += lowercase;
+  }
+  if (numCase) {
+    multiSelect += numbers;
+  }
+  if (specialCase) {
+    multiSelect += special;
+  }
+  let confirmedPass = "";
+  for (i = 0; i < passLength; i++) {
+    let range = [Math.floor(Math.random() * multiSelect.length)];
+    confirmedPass = confirmedPass + multiSelect[range];
+  }
+  return confirmedPass;
+};
+
+// Write password to the #password input (provided)
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button (provided)
+generateBtn.addEventListener("click", writePassword);
---
+++ Challenge3/Challenge3/friendly-parakeet/README.md
@@ -0,0 +1,17 @@
+Password Generator Project
+
+Password generator that has preset values for capital and lower case letters, numbers, and pre-determined special characters.  User inputs the quantity of characters they would like their password to be and the generator will deliver it.
+
+Criteria is set between 8-128 letters long and the user must input a number in this range.  If outside the category or NaN, it will prompt the user of the criteria/what they need to do to get to the confirm messages for the rest of the criteria.
+
+First prompt after clicking generate password.
+![Alt text](image.png)
+
+Final prompt after entering "10" and clicking generate password.
+![Alt text](image-1.png)
+
+Github link:
+https://github.com/NatEngs/Challenge3
+
+Live URL link:
+
